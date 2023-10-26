6497ae082726c9dff272e58ad5cc197392b6ac98
Aman Wolde <aman.wolde@valpo.edu>
2023-10-26 18:17:31 +0000

    test 1

--- createvm.sh
+++ createvm.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-gcloud compute instances create $VM_NAME --zone us-central1-a --machine-type=e2-medium  --network-interface=network=custom-vpc,subnet=us-central-subnet --tags=web-server --service-account=vm-sa-730@aw-project-374412.iam.gserviceaccount.com --scopes=cloud-platform
+echo $1 > token
+VM_NAME=ci-vm
+gcloud compute instances create $VM_NAME --zone us-central1-a --machine-type=e2-medium  --network-interface=network=custom-vpc,subnet=us-central-subnet --tags=web-server --service-account=vm-sa-730@aw-project-374412.iam.gserviceaccount.com --scopes=cloud-platform  --access-token-file=token
